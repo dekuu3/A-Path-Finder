40cd7e5290e781226ae84abf16d6891d20469d67
dekuu3 <davidpireslazaro@gmai.com>
2019-11-12 23:40:32 +0000

    Output method will now write solution to args[2]

--- APathFinder/APathFinder/Program.cs
+++ APathFinder/APathFinder/Program.cs
@@ -9,33 +9,40 @@ namespace APathFinder
 {
     class Program
     {
+        #region Main
         static void Main(string[] args)
         {
             var inputFile = args[0];
             var outputFile = args[1];
+            var solution = "0";
 
             //Converting the input file into an int[]
             int[] inputFileText = Array.ConvertAll(File.ReadAllText(inputFile).Split(','), s => int.Parse(s));
 
             //Checking if the input text is in the right format (Length == N + N*2 + N*N)
-            IsInputValid(inputFileText, inputFileText.Length);
+            var isInputValid = IsInputValid(inputFileText, inputFileText.Length);
+            Output(isInputValid, outputFile, solution);
 
             //Checking if the binary matrix provided is correct (all numbers must be either 0 or 1)
-            IsMatrixValid(inputFileText);
+            var isMatrixValid = IsMatrixValid(inputFileText);
+            Output(isMatrixValid, outputFile, solution);
         }
+        #endregion
 
-        private static void IsInputValid(int[] inputFileText, int inputLength)
+        #region Input File Validation
+        private static int IsInputValid(int[] inputFileText, int inputLength)
         {
             if (inputLength != (1 + (inputFileText[0] * 2) + (inputFileText[0] * inputFileText[0])))
             {
                 //Format/length isn't valid
-                int outputVar = 0;
-
-                Output(outputVar);
+                return 0;
             }
+            return 3;
         }
+        #endregion
 
-        private static void IsMatrixValid(int[] inputFileText)
+        #region Matrix Validation
+        private static int IsMatrixValid(int[] inputFileText)
         {
             var matrixStartIndex = 1 + inputFileText[0]*2;
 
@@ -44,30 +51,35 @@ namespace APathFinder
                 if (!(inputFileText[i] == 0 || inputFileText[i] == 1))
                 {
                     //Binary matrix isn't valid
-                    int outputVar = 0;
-
-                    Output(outputVar);
+                    return 0;
                 }
             }
+            return 3;
         }
+        #endregion
 
-        private static void Output(int outputVar)
+        #region Output
+        private static void Output(int outputVar, string outputFile, string solution)
         {
             switch (outputVar)
             {
+                case 0:
+                    //output error message & force program to quit
+                    File.WriteAllText(outputFile, "Input file format not valid");
+                    Environment.Exit(0);
+                    break;
                 case 1:
                     //no error = found solution
+                    File.WriteAllText(outputFile, solution);
                     break;
                 case 2:
                     //no error = found NO solution
+                    File.WriteAllText(outputFile, solution);
                     break;
                 default:
-                    //output error message
-                    //force program to stop
-                    Console.WriteLine("nop");
-                    Environment.Exit(0);
                     break;
             };
         }
+        #endregion
     }
 }
