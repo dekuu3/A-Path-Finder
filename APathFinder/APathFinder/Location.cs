1339b955d5541b80747ce73c2161f29f8555511f
dekuu3 <davidpireslazaro@gmai.com>
2019-11-15 00:23:24 +0000

    Added methods to calculate Target coords

--- APathFinder/APathFinder/Location.cs
+++ APathFinder/APathFinder/Location.cs
@@ -8,15 +8,5 @@
         public int G;
         public int H;
         public Location Parent;
-
-        public Location(int x, int y, int f, int g, int h, Location parent)
-        {
-            X = x;
-            Y = y;
-            F = f;
-            G = g;
-            H = h;
-            Parent = parent;
-        }
     }
 }
